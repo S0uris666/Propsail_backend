a0152e730e384b45d9c366a6dc2cc06649ce3e53
unknown <pihenriquez.dwfs19@bootcampudd.cl>
2025-11-28 12:39:06 -0300

    Corrección: eliminación de importaciones de PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './infra/database/prisma.module';
 import { EmailModule } from './infra/mail/email.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AppConfigModule } from './config/app-config.module';
+import { PrismaModule } from './infra/database/prisma.module';
 
 @Module({
   imports: [AppConfigModule, PrismaModule, EmailModule, AuthModule],
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaModule } from '../../infra/database/prisma.module';
 import { EmailModule } from 'src/infra/mail/email.module';
 
 @Module({
-  imports: [PrismaModule, EmailModule],
+  imports: [EmailModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
